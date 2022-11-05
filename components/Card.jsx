70c68d3cfcc6b533b635928f114725b5a3055fba
giohax <giovannifgonzaga@gmail.com>
2022-11-05 15:39:45 -0500

    add button layout and hover properties to card component

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Card = ({ children }) => {
     return (
-        <div className="bg-indigo-400 rounded-3xl p-10 pb-0 ">{children}</div>
+        <div className="bg-indigo-400 rounded-3xl p-10 pb-0 hover:scale-105 transition-transform duration-200 overflow-hidden cursor-pointer">
+            {children}
+        </div>
     );
 };
 
--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -7,10 +7,35 @@ const Portfolio = () => {
         <div className="flex justify-center">
             <div className="grid grid-cols-2 gap-5 p-5 pb-0 max-w-5xl">
                 <Card src="" alt="" title="" caption="">
-                    <h1 className="text-white text-lg">Google Search</h1>
-                    <p className="text-gray-300 mb-10 text-sm">
-                        NextJS application using Programmable Search Engine
-                    </p>
+                    <div className="flex justify-between select-none ">
+                        <div className="flex flex-col basis-1/2 ">
+                            <h1 className="text-white text-lg">
+                                Google Search
+                            </h1>
+                            <p className="text-gray-300 mb-10 text-sm">
+                                NextJS application using Programmable Search
+                                Engine
+                            </p>
+                        </div>
+
+                        <div className="bg-white h-10 w-10 flex justify-center items-center rounded-full active:bg-gray-400">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-4 h-4"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+
                     <Image
                         src="/googlesearch.png"
                         alt="Google Mockup"

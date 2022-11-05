6288e5d01a8d83fad4106575100565af1bce6795
giohax <giovannifgonzaga@gmail.com>
2022-11-05 16:19:29 -0500

    add images and colors to card components

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import Image from "next/image";
 
-const Card = ({ children }) => {
+const Card = ({ src, alt, title, caption, redirect, color, children }) => {
     return (
-        <div className="bg-indigo-400 rounded-3xl p-10 pb-0 hover:scale-105 transition-transform duration-200 overflow-hidden cursor-pointer">
-            {children}
+        <div
+            className={`${color} rounded-3xl p-10 pb-0 hover:scale-105 transition-transform duration-200 overflow-hidden cursor-pointer flex flex-col justify-between`}
+        >
+            <div className="flex justify-between select-none ">
+                <div className="flex flex-col basis-2/3 ">
+                    <h1 className="text-white text-lg">{title}</h1>
+                    <p className="text-gray-300 mb-10 text-sm">{caption}</p>
+                </div>
+
+                <div className="bg-white h-10 w-10 flex justify-center items-center rounded-full active:bg-gray-400">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-4 h-4"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                    </svg>
+                </div>
+            </div>
+
+            <Image
+                src={src}
+                alt={alt}
+                width={500}
+                height={500}
+                className="rounded-xl -mb-5"
+            />
+            {/* {children} */}
         </div>
     );
 };
--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,57 +1,35 @@
 import React from "react";
 import Card from "./Card";
-import Image from "next/image";
 
 const Portfolio = () => {
     return (
         <div className="flex justify-center">
-            <div className="grid grid-cols-2 gap-5 p-5 pb-0 max-w-5xl">
-                <Card src="" alt="" title="" caption="">
-                    <div className="flex justify-between select-none ">
-                        <div className="flex flex-col basis-1/2 ">
-                            <h1 className="text-white text-lg">
-                                Google Search
-                            </h1>
-                            <p className="text-gray-300 mb-10 text-sm">
-                                NextJS application using Programmable Search
-                                Engine
-                            </p>
-                        </div>
+            <div className="flex flex-col sm:grid grid-cols-2 gap-5 p-5 pb-0 max-w-5xl">
+                <Card
+                    src="/googlesearch.png"
+                    alt="Google Search"
+                    title="Google Search"
+                    caption="NextJS application using Programmable Search Engine"
+                    redirect="https://google-clone-giohax.vercel.app/"
+                    color="bg-indigo-400"
+                />
+                <Card
+                    src="/imdb.png"
+                    alt="IMDB"
+                    title="IMDB"
+                    caption="NextJS application using TMDB API"
+                    redirect="https://imdb-clone-giohax.vercel.app/"
+                    color="bg-neutral-700"
+                />
 
-                        <div className="bg-white h-10 w-10 flex justify-center items-center rounded-full active:bg-gray-400">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-4 h-4"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-                                />
-                            </svg>
-                        </div>
-                    </div>
-
-                    <Image
-                        src="/googlesearch.png"
-                        alt="Google Mockup"
-                        width={500}
-                        height={500}
-                        className="rounded-xl -mb-5"
-                    />
-                </Card>
-                <Card>
-                    <h1>IMDB</h1>
-                    <p>NextJS application using the TMDB Api</p>
-                </Card>
-                <Card>
-                    <h1>Searchbook</h1>
-                    <p>Socla media for poker players</p>
-                </Card>
+                <Card
+                    src="/searchbook.png"
+                    alt="Searchbook"
+                    title="Searchbook"
+                    caption="ReactJS application with Redux"
+                    redirect="https://searchbook-giohax.vercel.app/"
+                    color="bg-yellow-800"
+                />
             </div>
         </div>
     );
